f1d63d9c52a5e10bb65fad33da4c080f41da12f3
INdIE DeV <krishnendu.bhilai@gmail.com>
2021-08-16 11:48:07 +0530

    Initial Commit

--- Scenes/Bullets/ComapnionBullet.gd
+++ Scenes/Bullets/ComapnionBullet.gd
@@ -1,21 +1,6 @@
 extends Bullet
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
-
-
 func _physics_process(delta: float) -> void:
 	var collision_normal = $RayCast2D.get_collision_normal()
 	rotation_degrees = rad2deg(collision_normal.angle())
